05177bd8674b6e7ff2272dcccf4184a96f06a2ef
Mohamed Ashraf Tolba <mohamed.ashraf.memo@gmail.com>
2021-12-17 06:44:36 +0200

    main page v1.2 about page v1.0 contact page v1.0 and addin a loading wrapper

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// display the searchbar
 const searchBar = document.querySelector('section.searchbar'),
     searchTriger = document.querySelector('.searchTriger');
 
 searchTriger.addEventListener('click',()=>{showHidden(searchBar);});
-
+// display hidden item
 function showHidden(hiddenItem) {
     if(hiddenItem.classList.contains('hidden')){
         hiddenItem.classList.remove('hidden');
@@ -11,6 +12,7 @@ function showHidden(hiddenItem) {
     }
 }
 
+// change mode
 const chageMode = document.querySelector('.mode');
 var cssRoot = document.querySelector(':root');
 
@@ -24,3 +26,18 @@ chageMode.addEventListener('click',()=>{
         cssRoot.style.setProperty('--secondary', '#1F4E5F');
     }
 });
+
+//display the sidebar
+const sideBar = document.querySelector('.side-bar'),
+    sideTriger = document.querySelector('.sidebar-triger'),
+    sideclose = document.querySelector('.sidebar-close');
+
+    sideTriger.addEventListener('click',()=>{showHidden(sideBar);});
+    sideclose.addEventListener('click',()=>{showHidden(sideBar);});
+
+// remove the loading page when page is loaded
+const loaderContainer = document.querySelector('#loader-container');
+
+    window,addEventListener('DOMContentLoaded', ()=>{
+        loaderContainer.classList.add('loaded');
+    });
